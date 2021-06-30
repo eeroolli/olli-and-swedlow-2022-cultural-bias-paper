16a918630711a2667eb61b62044fc4e0107360a4
Eero Olli M720 <eero.olli@gmail.com>
2021-06-30 19:14:29 +0200

    Improved the readability of the create graphs.R Run all figures a new

--- create graphs.R
+++ create graphs.R
@@ -4,6 +4,9 @@
 # EO 30.10.2020
 # EO 09.11.2020
 
+
+
+#### Intro -------------
 remove(list = ls())
 
 library(tidyverse)
@@ -25,8 +28,10 @@ source("../Startup/eeros_functions.R")
 googledrive::drive_auth(email = "eero.olli@gmail.com")
 googlesheets4::gs4_auth(token = drive_token())
 
-setwd("../CT and LR with Brendon")
+setwd("../cultural-bias-and-left-right-paper")
 
+
+# Lists of party names must stay in same order
 
 partylist_l <- c("Socialist Left", "Social Democrat", "Conservative" ,
                  "Liberal",        "Agrarian"  ,      "Christian"   ,             
@@ -38,107 +43,25 @@ partylist_n <- c("lsoc",  "socdem", "cons"   ,
                  "progr", "green" )
 
 partylist <- c(
-    "socialist_left" ,     "soc_dem" ,     "conservative" ,
-    "liberal"     ,     "agrarian"    ,     "christian"  ,
-    "progress"   ,     "green"   )
+    "socialist_left" ,     "soc_dem"     ,     "conservative" ,
+    "liberal"        ,     "agrarian"    ,     "christian"  ,
+    "progress"       ,     "green"   )
 
-# #### GET DATA ----------
-# 
-# mydata <-
-#  read_from_gsheet(googlesheet_key = "1HHTxRCQ5_MnP5kPqDeEpyGwuIurmxCq60h6TTN7NwAw",
-#                  sheet_name_to_read = "Party_family_preference_by_CB_LR",
-#                  columnname_indicator = "Top 2 CB"
-#                  ) %>% 
-#   janitor::remove_empty(which = c("rows", "cols")) %>% 
-#   janitor::clean_names() %>% 
-#   hablar::retype() %>% 
-#   filter(!is.na(top_2_cb)) 
-# 
-# # remove non significant numbers in alternative data
-# mydata_na <- mydata
-# for (i in 2:ncol(mydata)) {
-#   for (j in 1:nrow(mydata)) {
-#     if (is.na(mydata[j, i]))  {
-#       mydata_na[j, i - 1] <- NA  # drop the value in the column on left side
-#     }
-#   }
-# }
-# 
-# # replace the NA in main
-# mydata <- mydata %>% 
-#   mutate(across(.cols = ends_with("_sig"), ~str_replace_na(., replacement = ""))) %>%  # Make all NA sig to "not sign."
-#   mutate(across(.cols = ends_with("_sig"), ~as_numeric(str_length(.)))) %>%
-#   # transmute(across(.cols = ends_with("_sig"), ~str_length(.))) %>%
-#   set_labels(., ends_with("_sig"), labels = c("non sign." = 0  , 
-#                               "0.05"      = 1        ,
-#                               "0.01"      = 2        ,
-#                               "0.001"     = 3   ) ,
-#             force.labels = TRUE ) %>% 
-#   as_factor(., ends_with("_sig"), add.non.labelled = TRUE) 
-# 
-# str(mydata)
-# names(mydata)
-# Hmisc::contents(mydata)
-# 
-# 
-# # make labels-factors
-#   mydata <- 
-#   rename(mydata, 
-#          socialist_left_sig = sl_sig,
-#          soc_dem_sig = sd_sig,
-#          agrarian_sig = ag_sig,
-#          liberal_sig = lib_sig,
-#          christian_sig = chr_sig,
-#          conservative_sig = con_sig,
-#          progress_sig = pro_sig,
-#          green_sig = gr_sig,
-#          other_sig = o_sig) 
-#              
-# 
-# str(mydata)  
-# names(mydata)
-# Hmisc::contents(mydata)
-# 
-# 
-# 
-# #### All 19 Cases -------------
-# # Totparty19 <- mydata_na %>%     # only the signifcant cases
-# Totparty19 <- mydata %>% # All 19 cases
-#   dplyr::slice_max(., order_by = n, n = 19) %>% 
-#   remove_col_if_ends_with("sig") %>% 
-#   arrange(mean_left_right)
-# 
-# summarise(Totparty19, n, top_2_cb, mean_left_right )  # a check   
-# 
-# 
-# names(Totparty19)
-# write.csv2(Totparty19, "Totparty19_preference_by_cultural_bias_sig.csv")
-# 
-# # Totparty19 <-  read.csv2("Totparty19_preference_by_cultural_bias_sig.csv")  
-# 
-# # 
-# #names(Totparty19[2:9]  # these are in wrong order!
-# 
-# 
-# #### All 57 cases are used to create the LloWess Soothed line
-# 
-# Totparty57 <- mydata %>%
-#   dplyr::slice_max(., order_by = n, n = 57) %>% 
-#   arrange(mean_left_right) 
-# 
-# summarise(Totparty57, n, top_2_cb, mean_left_right )  # a check   
-# 
-# 
-# names(Totparty57)
-# write.csv2(Totparty57, "Totparty57_preference_by_cultural_bias_sig.csv")
 
-# Totparty57 <-  read.csv2("Totparty57_preference_by_cultural_bias_sig.csv")  
 
+#### READ the DATA
 
 
 Totparty19 <- read.csv2("Totparty19_preference_by_cultural_bias_sig.csv")   %>%
   hablar::retype() %>% 
-  select(!starts_with("X")) 
+  select(!starts_with("X")) %>% 
+  set_labels(., ends_with("_sig"), 
+             labels = c("non sign." = 0        ,
+                        "0.05"      = 1        ,
+                        "0.01"      = 2        ,
+                        "0.001"     = 3   )    ,
+             force.labels = TRUE ) %>%
+  as_factor(., ends_with("_sig"), add.non.labelled = TRUE)
 
  
    
@@ -147,7 +70,15 @@ View(Totparty19)
 
 Totparty57 <- read_csv2("Totparty57_preference_by_cultural_bias_sig.csv") %>%  
   hablar::retype() %>% 
-  select(!starts_with("X")) 
+  select(!starts_with("X")) %>% 
+  set_labels(., ends_with("_sig"), 
+             labels = c("non sign." = 0        ,
+                        "0.05"      = 1        ,
+                        "0.01"      = 2        ,
+                        "0.001"     = 3   )    ,
+             force.labels = TRUE ) %>%
+  as_factor(., ends_with("_sig"), add.non.labelled = TRUE)
+
 
 View(Totparty57)
 
@@ -155,21 +86,23 @@ View(Totparty57)
 
 #### Data into Long format -----------
 
-Totparty19_long <-
-gather(Totparty19, 
-       socialist_left, soc_dem, agrarian  , liberal ,
-       christian , conservative, progress, green  , 
-       key = "party_family",
-       value = "support_pct" )
+Totparty19_long <- Totparty19 %>% 
+  select(!ends_with("sig")) %>%
+  select(!ends_with("sum")) %>%
+  pivot_longer( 
+    cols = socialist_left:other  , 
+    names_to = "party_family",
+    values_to = "support_pct" )
+  
 
 
-Totparty57_long <-
-  gather(Totparty57, 
-         socialist_left, soc_dem, agrarian  , liberal ,
-         christian , conservative, progress, green  , 
-         key = "party_family",
-         value = "support_pct" )
-
+Totparty57_long <- Totparty57 %>% 
+  select(!ends_with("sig")) %>%
+  select(!ends_with("sum")) %>%
+  pivot_longer( 
+    cols = socialist_left:other  , 
+    names_to = "party_family",
+    values_to = "support_pct" )
 
 
 
@@ -367,10 +300,6 @@ ggplot2::ggsave(
 #                   dpi = 300)  
 # }
 # 
-
-for (i in seq_along(partylist)) {
-   print(partylist[i])
-}
 
   
 
--- get_NORDIC_1999_datafile.R
+++ get_NORDIC_1999_datafile.R
@@ -1,7 +1,6 @@
 
-#The purpose is to create new figs to Party PReference using the full dataset
-# to create the function for line between LR and party preference.
 
+#The purpose is to create new figs to Party PReference.
 # EO 30.10.2020
 # EO 09.11.2020
 
@@ -11,328 +10,134 @@ library(tidyverse)
 library(sjmisc)
 library(sjlabelled)
 library(readxl)
-library(ggplot2)
 library(svMisc)
 library(janitor)
 library(hablar)
 library(googledrive)
 library(googlesheets4)
 library(dplyr)
+library(ggplot2)
+library(patchwork)
+
 
 source("../Startup/eeros_functions.R")
 
 googledrive::drive_auth(email = "eero.olli@gmail.com")
 googlesheets4::gs4_auth(token = drive_token())
 
-setwd("../cultural-bias-and-left-right-paper")
+setwd("../CT and LR with Brendon")
 
 
-
-
-#### GET DATA ----------
-
-mydata <- 
-  foreign::read.spss("E:/data/Nordic1999_Nordic_All3_tri3565_bin50_c10_n40_recoded.sav",
-                     to.data.frame = TRUE) %>% 
-  janitor::remove_empty(which = c("rows", "cols")) %>% 
-  janitor::clean_names() %>% 
-  hablar::retype() 
-
-mydata$left_right01 <- hablar::as_reliable_num(mydata$left_right01)
-#mydata$left_right01_b10 <- bin
-
-
-
-
-names(mydata)  
-str(mydata)
-get_labels(mydata$totparty)
-
-#recode()
-partylist_l <- c("Socialist Left", "Social Democrat", "Conservativ" ,
+partylist_l <- c("Socialist Left", "Social Democrat", "Conservative" ,
                  "Liberal",        "Agrarian"  ,      "Christian"   ,             
-                 "Progress" ,      "Greens")      
- 
+                 "Progress" ,      "Green")      
 
 
 partylist_n <- c("lsoc",  "socdem", "cons"   , 
                  "liber", "agrar" , "christ" , 
                  "progr", "green" )
 
+#### GET DATA ----------
 
-partylist_dummies <-  c("lsoc_dummy"  , "socdem_dummy",   "cons_dummy" ,
-                "liber_dummy" , "agrar_dummy"  ,  "christ_dummy" ,
-                "progr_dummy",  "green_dummy" ,  "dontknow_dummy" ,
-                "novote_dummy" ,  "noanswer_dummy"   )
+mydata <-
+  read_from_gsheet(googlesheet_key = "1HHTxRCQ5_MnP5kPqDeEpyGwuIurmxCq60h6TTN7NwAw",
+                   sheet_name_to_read = "Party_family_preference_by_CB_LR",
+                   columnname_indicator = "Top 2 CB"
+  ) %>% 
+  janitor::remove_empty(which = c("rows", "cols")) %>% 
+  janitor::clean_names() %>% 
+  hablar::retype() %>% 
+  filter(!is.na(top_2_cb)) 
 
-tabyl(mydata, "lsoc_dummy")
-
-for (i in seq_along(partylist_l)) {
-  mydata
+# remove non significant numbers in alternative data
+mydata_na <- mydata
+for (i in 2:ncol(mydata)) {
+  for (j in 1:nrow(mydata)) {
+    if (is.na(mydata[j, i]))  {
+      mydata_na[j, i - 1] <- NA  # drop the value in the column on left side
+    }
+  }
 }
-mydata 
 
-test <-   
-  mutate(mydata, ends_with("dummy"), 
-                       c("This Party" = "1", "Other party" = "0" ))
+# replace the NA in main
+mydata <- mydata %>% 
+  mutate(across(.cols = ends_with("_sig"), ~str_replace_na(., replacement = ""))) %>%  # Make all NA sig to "not sign."
+  mutate(across(.cols = ends_with("_sig"), ~as_numeric(str_length(.)))) %>%
+  # transmute(across(.cols = ends_with("_sig"), ~str_length(.))) %>%
+  set_labels(., ends_with("_sig"), labels = c("non sign." = 0  , 
+                                              "0.05"      = 1        ,
+                                              "0.01"      = 2        ,
+                                              "0.001"     = 3   ) ,
+             force.labels = TRUE ) %>% 
+  as_factor(., ends_with("_sig"), add.non.labelled = TRUE) 
 
-
-  
-
-linedata  <-  mydata  %>%
-  group_by(totparty)  %>% 
-  select(totparty,country, left_right01, lsoc:noanswer_dummy, cult_rank_tri_a2) %>% 
-  rename(top2bias = cult_rank_tri_a2) %>% 
-  filter(!totparty == "NA") %>% 
-  filter(!totparty == "Other") %>% 
-  filter(!is.na(left_right01)) 
-
+str(mydata)
+names(mydata)
 Hmisc::contents(mydata)
-  
 
-linedata$partyfam <- as.factor(linedata$totparty)
 
+# make labels-factors
+mydata <- 
+  rename(mydata, 
+         socialist_left_sig = sl_sig,
+         soc_dem_sig = sd_sig,
+         agrarian_sig = ag_sig,
+         liberal_sig = lib_sig,
+         christian_sig = chr_sig,
+         conservative_sig = con_sig,
+         progress_sig = pro_sig,
+         green_sig = gr_sig,
+         other_sig = o_sig) 
 
 
-recode_key <- c("this party" = "1", "other party" = "0")
-test <- plyr::revalue(linedata$lsoc_dummy, !!!recode_key) 
+str(mydata)  
+names(mydata)
+Hmisc::contents(mydata)
 
-str(linedata$lsoc_dummy)
-factor(linedata$lsoc_dummy)
-table(linedata$lsoc_dummy)
 
 
-countrydata  <-  mydata  %>%
-  select(totparty,country, left_right01, lsoc:noanswer_dummy, cult_rank_tri_a2) %>% 
-  rename(top2bias = cult_rank_tri_a2) %>% 
-  filter(!totparty == "NA") %>% 
-  filter(!totparty == "Other") %>% 
-  filter(!is.na(left_right01))
+#### All 19 Cases -------------
+# Totparty19 <- mydata_na %>%     # only the signifcant cases
+Totparty19 <- mydata %>% # All 19 cases
+  dplyr::slice_max(., order_by = n, n = 19) %>% 
+  remove_col_if_ends_with("sig") %>% 
+  arrange(mean_left_right)
 
+summarise(Totparty19, n, top_2_cb, mean_left_right )  # a check   
 
-countrydata %>%
- group_by(totparty, country) %>% 
- summarise(lr = mean(left_right01,
-           ps_n = n()
-           )
-           
-           
-           
-           ,
-            ps_n = count(totparty))
 
-country_data$lr_mean <- countrydata %>%  
-  mean(left_right01) 
+names(Totparty19)
+write.csv2(Totparty19, "Totparty19_preference_by_cultural_bias_sig.csv")
 
+# Totparty19 <-  read.csv2("Totparty19_preference_by_cultural_bias_sig.csv")  
 
-###### MODEL Multinominal Logistic ############ -----------------
+partylist <- c(
+  "socialist_left" ,
+  "soc_dem" ,
+  "conservative" ,
+  "liberal"     ,
+  "agrarian"    ,
+  "christian"  ,
+  "progress"   ,
+  "green"
+)
 
-# Using sample_frac to create 70 - 30 slipt into test and train
-train <- sample_frac(linedata, 0.7)
-sample_id <- as.numeric(rownames(train)) # rownames() returns character so as.numeric
-test <- linedata[-sample_id,]
+#names(Totparty19[2:9]  # these are in wrong order!
 
 
-# Setting the basline 
-train$partyfam <- relevel(train$partyfam, ref = "Social Democrat")
-train
+#### All 57 cases are used to create the LloWess Soothed line
 
+Totparty57 <- mydata %>%
+  dplyr::slice_max(., order_by = n, n = 57) %>% 
+  arrange(mean_left_right) 
 
-# Training the multinomial model
-multinom.fit <- nnet::multinom(partyfam ~ left_right01, data = train)
+summarise(Totparty57, n, top_2_cb, mean_left_right )  # a check   
 
-# Checking the model
-summary(multinom.fit)
 
-head(probability.table <- fitted(multinom.fit))
+names(Totparty57)
+write.csv2(Totparty57, "Totparty57_preference_by_cultural_bias_sig.csv")
 
-
-# Predicting the values for train dataset
-train$predicted <- predict(multinom.fit, newdata = train, "class")
-tabyl(train, predicted)
-
-# Building classification table
-ctable <- table(train$partyfam, train$predicted)
-
-# Calculating accuracy - sum of diagonal elements divided by total obs
-round((sum(diag(ctable))/sum(ctable))*100,2)
-
-### Test ------------
-# Predicting the values for train dataset
-test$predicted <- predict(multinom.fit, newdata = test, "class")
-
-# Building classification table
-ctable <- table(test$partyfam, test$predicted)
-
-# Calculating accuracy - sum of diagonal elements divided by total obs
-round((sum(diag(ctable))/sum(ctable))*100,2)
-print("This is a bad model. very low predicting ability")
-
-
-
-
-
-##LEARN purrr::map ###########-----------------------------------------  
-linedata %>%
-#   LEARN purrr::map(
-#     
-#     
-#     
-#   )
-# map_dfr(  
-# case_when(
-#   str_detect(linedata$lsoc_dummy, "this party")  ~ 1L ,
-#   str_detect(linedata$lsoc_dummy, "other party") ~ 0L ,
-#   TRUE                                           ~ NA_integer_ )
-# 
-#   char_vec <- sample(c("a", "b", "c"), 10, replace = TRUE)
-#   level_key <- c(a = "apple", b = "banana", c = "carrot")
-#   recode(char_vec, !!!level_key)
-  
-  
-  
-
-
-
-tabyl(linedata, "partyfam")  
-  
-tabyl(mydata, "totparty")  
-
-  
-
-# recode(mydata, partylist_dummies, lowest = 0, highest = 1)
-
-
-############# GGPLOT2 ------------------------------------------- 
-str(linedata)
-party_family_support <- tibble(partyname = NA_character_, 
-                               support_pct = NA_real_,
-                               left_right01 = NA_real_,
-                               country = NA_character_,
-                               .rows = 8)
-
-for (i in seq_along(partylist_n)) {
-  party_family_support$partyname[[i]] <- partylist_l[[i]]
-  n_of_cases <- nrow(linedata)
-  support_n <- 
-    length(which(linedata$totparty == partylist_l[i] ))
-  party_family_support$support_pct[[i]]  <- support_n / n_of_cases * 100
-  party_family_support$left_right01[[i]] <- 
-     mean(linedata$left_right01[linedata$totparty == partylist_l[i]])
-  }   
-
-country_party_family <- mydata %>% 
-  group_by(totparty, country) %>% 
-  mutate(
-    
-  )
-  spread(mydata, key ) 
-
-party_family_support
-
-for (i in seq_along(partylist_n)) {
-  party <- partylist_l[[i]]
-  print(party)
-  n_of_cases <- nrow(linedata)
-  print(n_of_cases)
-  support_n <- length(which(linedata$totparty == partylist_l[i] ))
-  support_pct <- support_n / n_of_cases * 100
-  print(support_pct)
-  linedata$support_pct <-
-  linedata %>% 
-     filter(str_detect(totparty,partylist_l[i])) %>% 
-     mutate(support_pct = length(which(totparty ==partylist_l[i] )) / n_of_cases) %>% 
-     select(left_right01, support_pct) 
-  print(linedata$support_pct)   
-}   
-  
-
-
-ggplot(party_family_support, aes(left_right01, support_pct)) + 
-  ggplot2::geom_point(size = 4) +
-  ggrepel::geom_label_repel(aes(label = partyname)) +
-  labs(title = "Which Party would you vote for if there was a parliamentary election today? ")  +
-  xlab("Mean Left-Right Position") +
-  ylab("Party Family Supporters (percent)" )
-  
-
-for (i in seq_along(partylist_l)) {
-  mydata %>% 
-    filter(str_detect(totparty,partylist_l[i])) %>% 
-    ggplot(party_family_support, aes(left_right01, support_pct)) + 
-    ggplot2::geom_point(aes(shape = partyname)) +
-    labs(title = str_to_title(partylist_l[i]))  +
-    xlab("Mean Left-Right Position") +
-    ylab("Party Family Supporters (percent)") +
-    #  scale_shape_manual(values = c(1,4,22)) +
-    ggrepel::geom_label_repel(aes(label = top_2_cb))        +
-    ggplot2::stat_smooth(data = linedata,
-                         method = "loess", 
-                         fullrange = FALSE,
-                         se = FALSE,
-                         span = 4) +
-    theme_gray()
-  
-  print(partylist_l[i])
-
-  
-  ggplot2::ggsave(filename = paste0(partylist_n[i],"_ind_linedata.png"),
-                  width = 12,
-                  units = "cm",
-                  dpi = 300)  
-}
-
-
-ggplot2::ggplot(linedata, aes(left_right01)) + 
-  stat_count(left_right01, geom = "bar") +
-  ggplot2::geom_bar(position = "fill") +
-  xlab("Left-Right Self Placement") +
-  ylab("Party Family Preference")  
-
-
-# soc_dem  
-ggplot2::ggplot(linedata, aes(left_right01, totparty)) + 
-  ggplot2::geom_violin(scale = "area" ) +
-  xlab("Left-Right Self Placement") +
-  ylab("Party Family Preference")  
-
-ggplot2::ggplot(linedata, aes(left_right01, totparty)) + 
-  ggplot2::geom_boxplot(outlier.shape = NA,
-                        notch = FALSE) +
-  xlab("Left-Right Self Placement") +
-  ylab("Party Family Preference") 
-
-
-ggplot2::ggplot(linedata, aes(left_right01, totparty)) + 
-  ggplot2::geom_count() +
-  ggplot2::scale_size_area()
-
-
-
-
-
-
-
-
-ggplot2::ggplot(linedata, aes(left_right01, totparty)) + 
-  ggplot2::geom_dotplot(binaxis = totparty) 
-
-
-
-ggplot2::ggplot(linedata, aes(left_right01, totparty)) + 
-  ggplot2::geom_pointrange() +  
-  geom_jitter(height = 0.2, width = 0)
-
-  ggplot2::geom_line(color = "grey", aes(group = n))               +
-  ggplot2::labs(x = "Mean Left-Right Position",
-                y = "Social Democratic Party Family Supporters (percent)",
-                caption = "Only significant deviations in party preference are included.")  
-
-ggplot2::ggsave(filename = "soc_dem LF.png",
-                width = 12,
-                units = "cm",
-                dpi = 300)  
-
+# Totparty57 <-  read.csv2("Totparty57_preference_by_cultural_bias_sig.csv")  
 
 
 
